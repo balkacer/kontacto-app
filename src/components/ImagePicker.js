78406b4efdbd31bcc0ac4ceff6eec78e5e48a3a7
Enmanuel Balcacer <makuttico@gmail.com>
2021-06-18 01:40:12 -0400

    Using ImagePicker as component

---
+++ src/components/ImagePicker.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { Image, Platform, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Color, Style } from "../tools";
+
+export default function ImagePickerComponent() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  return (
+    <TouchableOpacity onPress={pickImage}>
+        <Image
+            style={Style.image}
+            source={{ uri: image }}
+            defaultSource={require('../../assets/img/default.png')}
+        />
+    </TouchableOpacity>
+  );
+}
