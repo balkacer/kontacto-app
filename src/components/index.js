bbfac749268c77ef7ce206011e27ac3fea26d9dc
Enmanuel Balcacer <makuttico@gmail.com>
2021-06-18 01:42:11 -0400

    Adding ImagePicker to index of components

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,6 @@ export const Button = ButtonComponent;
 
 import FormMultipartsComponent from "./FormMultiparts";
 export const FormMultiparts = FormMultipartsComponent;
+
+import ImagePickerComponent from "./ImagePicker";
+export const ImagePicker = ImagePickerComponent;
