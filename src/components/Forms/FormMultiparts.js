61342f87ccca45f7bec6943bcba95570516a8f57
Enmanuel Balcacer <makuttico@gmail.com>
2021-07-05 02:38:50 -0400

    Using new FormInput component and fixing submit btn problem

--- src/components/Forms/FormMultiparts.js
+++ src/components/Forms/FormMultiparts.js
@@ -1,154 +1,80 @@
 import React, { Component } from 'react';
 import {
   View,
-  ScrollView,
   Text,
-  TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView
 } from 'react-native';
 import { Style } from '../../tools';
-import Theme from '../../theme/theme';
 import FormValidator from './FormValidator';
+import FormInput from './FormInput';
 
 export default class FormMultipartComponent extends Component {
-  currentFormPartPosition = 0;
+  currentPosition = 0;
   numItems = this.props.items.length;
+
+  state = {
+    currentPosition: []
+  }
 
   constructor(props) {
     super(props);
-
-    this.state = {}
-
-    this.state.currentFormPart = [];
-
-    this.props.items.map((item) => {
-      item.map((input) => {
-        this.state[input.property] = input.valueType === 'number' ? 0 : '';
-      });
-    });
   }
 
   showCurrentFormPart() {
-    const formInputsArray = [];
+    const form = [];
 
-    const formPartIndex = this.currentFormPartPosition;
-
-    this.props.items[formPartIndex].map((input, inputIndex) => {
-      const inputOfType = {
-        number: (
-          <TextInput
-            style={Style.input}
-            onChangeText={(value) => this.setState({ [input.property]: Number(value) })}
-            value={this.state[input.property]}
-            placeholderTextColor={Theme.colorInputsPlaceholder}
-            placeholder={input.label}
-            keyboardType='numeric'
-          />
-        ),
-        name: (
-          <TextInput
-            style={Style.input}
-            onChangeText={(value) => this.setState({ [input.property]: value })}
-            value={this.state[input.property]}
-            placeholderTextColor={Theme.colorInputsPlaceholder}
-            placeholder={input.label}
-            autoCapitalize='words'
-            textContentType='name'
-          />
-        ),
-        username: (
-          <TextInput
-            style={Style.input}
-            onChangeText={(value) => this.setState({ [input.property]: value })}
-            value={this.state[input.property]}
-            placeholderTextColor={Theme.colorInputsPlaceholder}
-            placeholder={input.label}
-            autoCompleteType='username'
-            textContentType='username'
-          />
-        ),
-        email: (
-          <TextInput
-            style={Style.input}
-            onChangeText={(value) => this.setState({ [input.property]: value })}
-            value={this.state[input.property]}
-            placeholderTextColor={Theme.colorInputsPlaceholder}
-            placeholder={input.label}
-            autoCompleteType='email'
-            textContentType='emailAddress'
-            keyboardType='email-address'
-          />
-        ),
-        password: (
-          <TextInput
-            style={Style.input}
-            onChangeText={(value) => this.setState({ [input.property]: value })}
-            value={this.state[input.property]}
-            placeholderTextColor={Theme.colorInputsPlaceholder}
-            placeholder={input.label}
-            autoCompleteType='password'
-            textContentType='password'
-            secureTextEntry
-          />
-        ),
-        date: (
-          <TextInput
-            style={Style.input}
-            onChangeText={(value) => this.setState({ [input.property]: value })}
-            value={this.state[input.property]}
-            placeholderTextColor={Theme.colorInputsPlaceholder}
-            placeholder={input.label}
-          />
-        ),
-      };
-
-      const inputItem = (
-        <View style={{ width: '100%' }} key={`form-control-${formPartIndex}-${inputIndex}`}>
-          {inputOfType[input.valueType]}
+    this.props.items[this.currentPosition].map((item, inputIndex) => {
+      const input = (
+        <View style={{ width: '100%' }} key={`form-control-${this.currentPosition}-${inputIndex}`}>
+          <FormInput control={item} />
         </View>
       );
 
-      formInputsArray.push(inputItem);
+      form.push(input);
     });
 
-    const currentFormPart = <View style={[Style.column_100]}>{formInputsArray}</View>;
+    const currentFormPart = <View style={[Style.column_100]}>{form}</View>;
 
     this.setState({ currentFormPart });
   }
 
-  onSubmit() {
-    const data = Object.assign({}, this.state);
-    delete data['currentFormPart'];
-    const formIsValid = this.formPartIsValid();
+  getFormData() {
+    const data = {}
 
-    if (formIsValid) {
-      return data
-    }
-  }
-
-  formPartIsValid() {
-    const form = this.props.items[this.currentFormPartPosition];
-
-    form.map(control => {
-      control.value = this.state[control.property];
+    this.props.items.map(form => {
+      form.map(control => {
+        if (!control.notToSubmit) {
+          data[control.property] = control.value;
+        }
+      });
     });
 
+    return data;
+  }
+
+  onSubmit() {
+    const data = this.getFormData();
+    const formIsValid = this.formIsValid();
+    return formIsValid ? data : null;
+  }
+
+  formIsValid() {
+    const form = this.props.items[this.currentPosition];
     return new FormValidator(form).formIsValid();
   }
 
   toPreviousPart = () => {
-    if (this.currentFormPartPosition !== 0) {
-      this.currentFormPartPosition--;
+    if (this.currentPosition !== 0) {
+      this.currentPosition--;
       this.showCurrentFormPart();
     }
   }
 
   toNextPart = () => {
-    const formIsValid = this.formPartIsValid();
-    console.log(formIsValid);
-
-    if (this.currentFormPartPosition !== this.numItems - 1 && formIsValid) {
-      this.currentFormPartPosition++;
+    const formIsValid = this.formIsValid();
+    if (this.currentPosition !== this.numItems - 1 && formIsValid) {
+      this.currentPosition++;
       this.showCurrentFormPart();
     }
   }
@@ -159,64 +85,22 @@ export default class FormMultipartComponent extends Component {
 
   render() {
     return (
-      <ScrollView
-        vertical
-        showsVerticalScrollIndicator={false}
-        pagingEnabled
-        style={{ width: '100%' }}
-      >
+      <ScrollView vertical showsVerticalScrollIndicator={false} pagingEnabled style={{ width: '100%' }}>
         <View style={Style.formPartView}>
           <View style={Style.row}>{this.state.currentFormPart}</View>
-          <View
-            style={[
-              Style.row,
-              Style.btnContainer,
-              { justifyContent: 'space-between' },
-            ]}
-          >
-            <TouchableOpacity
-              style={[
-                Style.btn,
-                { width: '47%' },
-                this.currentFormPartPosition === 0 ? Style.btnDisabled : Style.btnSecondary,
-              ]}
-              onPress={this.currentFormPartPosition === 0 ? () => { } : this.toPreviousPart}
-            >
-              <Text
-                style={[
-                  Style.btnText,
-                  this.currentFormPartPosition === 0 ? Style.btnDisabledText : {},
-                ]}
-              >
-                Prev
-              </Text>
+          <View style={[Style.row, Style.btnContainer, { justifyContent: 'space-between' }]}>
+            <TouchableOpacity style={[Style.btn, this.currentPosition === 0 ? Style.btnDisabled : Style.btnSecondary, { width: '47%' }]}
+              onPress={this.currentFormPartPosition === 0 ? () => { } : this.toPreviousPart}>
+              <Text style={[Style.btnText, this.currentFormPartPosition === 0 ? Style.btnDisabledText : {}]}>Prev</Text>
             </TouchableOpacity>
-            {this.currentFormPartPosition !== this.numItems - 1
-              ? <TouchableOpacity
-                style={[
-                  Style.btn,
-                  Style.btnSecondary,
-                  { width: '47%' },
-                ]}
-                onPress={this.toNextPart}
-              >
-                <Text style={Style.btnText}>
-                  Next
-                </Text>
-              </TouchableOpacity>
-              : <TouchableOpacity
-                style={[
-                  Style.btn,
-                  Style.btnPrimary,
-                  { width: '47%' },
-                ]}
-                onPress={this.props.onSubmit(this.onSubmit())}
-              >
-                <Text style={Style.btnText}>
-                  {this.props.lastBtnCaption}
-                </Text>
-              </TouchableOpacity>
-            }
+            {this.currentPosition !== this.numItems - 1 &&
+              <TouchableOpacity style={[Style.btn, Style.btnSecondary, { width: '47%' }]} onPress={this.toNextPart}>
+                <Text style={Style.btnText}>Next</Text>
+              </TouchableOpacity>}
+            {this.currentPosition === this.numItems - 1 &&
+              <TouchableOpacity style={[Style.btn, Style.btnPrimary, { width: '47%' }]} onPress={() => this.props.onSubmit(this.onSubmit())}>
+                <Text style={Style.btnText}>{this.props.submitBtnCaption}</Text>
+              </TouchableOpacity>}
           </View>
         </View>
       </ScrollView>
